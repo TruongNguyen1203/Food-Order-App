48c09d21b176891294c5ba69802c6b71480ddc68
TruongNguyen1203 <nguyentruong.120300@gmail.com>
2022-12-30 01:51:08 +0700

    handle error

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,33 @@
 import Card from "../UI/Card/Card";
 import classes from "./Cart.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../../store/CartContext";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const cartContext = useContext(CartContext);
+  const [isCheckout, setIsCheckout] = useState(false);
 
   const addItemToCart = (item) => {
-      const updatedItem = {...item, amount: 1}
-      console.log({...updatedItem});
+    const updatedItem = { ...item, amount: 1 };
+    console.log({ ...updatedItem });
 
-      cartContext.addItemToCart(updatedItem);
+    cartContext.addItemToCart(updatedItem);
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+    props.onOrderCart();
   }
+
+  const cancelOrderHandler = () => {
+    setIsCheckout(false);
+  }
+
   return (
     <div className={classes.backdrop}>
       <Card className={classes.card}>
-        <div className={classes['cart-container']}>
+        <div className={classes["cart-container"]}>
           {cartContext.items.map((item, index) => (
             <div key={item.id} className={classes["item-container"]}>
               <div className={classes["item-info"]}>
@@ -25,7 +37,9 @@ const Cart = (props) => {
                 </p>
               </div>
               <div className={classes["item-btn"]}>
-                <button onClick={() => cartContext.removeItemFromCart(item.id)}>-</button>
+                <button onClick={() => cartContext.removeItemFromCart(item.id)}>
+                  -
+                </button>
                 <button onClick={() => addItemToCart(item)}>+</button>
               </div>
             </div>
@@ -35,14 +49,23 @@ const Cart = (props) => {
           <h3>Total Amount</h3>
           <h3>${cartContext.totalAmount.toFixed(2)}</h3>
         </div>
-        <div className={classes["btn-control"]}>
-          <button className={classes["btn-close"]} onClick={props.onCloseCart}>
-            Close
-          </button>
-          <button className={classes["btn-order"]} onClick={props.onOrderCart}>
-            Order
-          </button>
-        </div>
+        {isCheckout && <Checkout onCancelOrder={cancelOrderHandler}></Checkout>}
+        {!isCheckout && (
+          <div className={classes["btn-control"]}>
+            <button
+              className={classes["btn-close"]}
+              onClick={props.onCloseCart}
+            >
+              Close
+            </button>
+            <button
+              className={classes["btn-order"]}
+              onClick={orderHandler}
+            >
+              Order
+            </button>
+          </div>
+        )}
       </Card>
     </div>
   );
